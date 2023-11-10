de85de060eb8e69dc1e7fc74a8c7a59d1fee6a91
LorehM18 <looreh18@hotmail.com>
2023-11-09 19:43:59 -0500

    Backend taller

--- punto 2/controller/controller.php
+++ punto 2/controller/controller.php
@@ -1,12 +1,14 @@
 <?php
 $message="";
 
-$ages=$_POST["age"];
+if($ages=isset($_POST["age"])){
+   $age=$_POST["age"];
+}
 
-
-if($ages>=18){
+if($age>=18){
     $message="Es mayor de edad";
 }else{
     $message="No es mayor de edad";
 }
+
 ?>
